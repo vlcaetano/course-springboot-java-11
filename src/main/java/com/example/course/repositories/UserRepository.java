012bb851922e95ced26f37e775cf7eb480bf6282
Vitor Caetano <vitorlc95@gmail.com>
2020-06-29 11:45:38 -0300

    Service layer, component registration

--- src/main/java/com/example/course/repositories/UserRepository.java
+++ src/main/java/com/example/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.course.entities.User;
 
+//como já está herdando do jparepository, não é necessário o @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
